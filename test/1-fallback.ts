63c592c0d60d16e478cbc0ac07ebc3e273d4b5b5
Sergi Juanati <wint3rmute@gmail.com>
2021-03-13 07:24:44 +0100

    Fixed TypeScript in tests

--- test/1-fallback.ts
+++ test/1-fallback.ts
@@ -1,41 +1,48 @@
 
-const { expectRevert } = require('@openzeppelin/test-helpers');
-const Fallback = require('Fallback.sol');
+import { accounts, contract } from '@openzeppelin/test-environment';
+const { balance } = require('@openzeppelin/test-helpers');
+import assert = require('assert');
 const web3 = require('web3');
 
 const ethToWei = (item: string) => web3.utils.toWei(item);
 
-let contract: any;
+// Ethereum accounts used in these tests
+const [
+    owner, // Contract owner
+    user1, // Random user
+] = accounts;
 
-contract('Fallback', (accounts: string[]) => {
-    let fallback: any;
-    let assert: any;
-    const [owner, user1] = accounts;
+// Loads a compiled contract using OpenZeppelin test-environment
+const Fallback = contract.fromArtifact('Fallback');
+let fallback: any;
 
-    beforeEach(async () => {
-        fallback = await Fallback.new({from: owner});
-    });
 
-    it('claim ownership & reduce contract balance to 0', async () => {
-
-        // Send some amount to pass 'contributions[msg.sender] > 0)'
-        await fallback.contribute({ from: user1, value: ethToWei('0.0005') });
-
-        // Send some amount to non existing function to access the callback function
-        // to get ownership
-        await fallback.sendTransaction({ from: user1, value: ethToWei('0.0001') });
-
-        // Check contract balance BEFORE
-        const balanceBefore = await web3.eth.getBalance(fallback.address);
-
-        // Withdraw all funds
-        await fallback.withdraw({ from: user1 });
-
-        // Check contract balance AFTER
-        const balanceAfter = await web3.eth.getBalance(fallback.address);
-
-        assert(balanceBefore === ethToWei('0.0006'));
-        assert(balanceAfter === '0');
-    });
-
+beforeEach(async () => {
+    fallback = await Fallback.new({from: owner});
 });
+
+it('claim ownership & reduce contract balance to 0', async () => {
+
+    // Send some amount to pass 'contributions[msg.sender] > 0)'
+    await fallback.contribute({ from: user1, value: ethToWei('0.0005') });
+
+    // Send some amount to non existing function to access the callback function
+    // to get ownership
+    await fallback.sendTransaction({ from: user1, value: ethToWei('0.0001') });
+
+    // Check contract balance BEFORE
+    const balanceBefore = await balance.current(fallback.address);
+
+
+    // Withdraw all funds
+    await fallback.withdraw({ from: user1 });
+
+    // Check contract balance AFTER
+    const balanceAfter = await balance.current(fallback.address);
+
+    assert(balanceBefore.toString() === ethToWei('0.0006'), 'Error 1');
+    assert(balanceAfter.toString() === '0', 'Error 2');
+});
+
+
+
